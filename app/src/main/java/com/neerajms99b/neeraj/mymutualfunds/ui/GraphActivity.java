a4dfa142f891c0047e858abf0afaff298fd24d59
neerajms <neerajms99b@gmail.com>
2016-10-05 15:55:06 +0530

    Back button implemented for graph activity

--- app/src/main/java/com/neerajms99b/neeraj/mymutualfunds/ui/GraphActivity.java
+++ app/src/main/java/com/neerajms99b/neeraj/mymutualfunds/ui/GraphActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.neerajms99b.neeraj.mymutualfunds.R;
 
@@ -13,6 +15,8 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_graph_activity);
+        setSupportActionBar(toolbar);
         Intent intent = getIntent();
         String scode = intent.getExtras().getString(getString(R.string.key_scode));
         String fundName = intent.getExtras().getString(getString(R.string.key_fundname));
@@ -20,14 +24,24 @@ public class GraphActivity extends AppCompatActivity {
         String units = intent.getExtras().getString(getString(R.string.key_units_in_hand));
         GraphFragment fragment = new GraphFragment();
         Bundle bundle = new Bundle();
-        bundle.putString(getString(R.string.key_scode),scode);
-        bundle.putString(getString(R.string.key_fundname),fundName);
-        bundle.putString(getString(R.string.key_fund_nav),fundNav);
-        bundle.putString(getString(R.string.key_units_in_hand),units);
+        bundle.putString(getString(R.string.key_scode), scode);
+        bundle.putString(getString(R.string.key_fundname), fundName);
+        bundle.putString(getString(R.string.key_fund_nav), fundNav);
+        bundle.putString(getString(R.string.key_units_in_hand), units);
         fragment.setArguments(bundle);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.graph_fragment,fragment);
-//        transaction.addToBackStack(null);
+        transaction.replace(R.id.graph_fragment, fragment);
         transaction.commit();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
